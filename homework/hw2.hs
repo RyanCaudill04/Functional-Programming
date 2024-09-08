65aa0dbece8f0448505a7d7ed5966f11371f0765
Ryan Caudill <rcaudill@email.sc.edu>
2024-09-08 11:45:55 -0400

    Pascal's triangle problem

--- homework/hw2.hs
+++ homework/hw2.hs
@@ -14,7 +14,7 @@ xs = map (2^) [0..]
 -- E.g., take 10 ys == [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1],[1,6,15,20,15,6,1],[1,7,21,35,35,21,7,1],[1,8,28,56,70,56,28,8,1],[1,9,36,84,126,126,84,36,9,1]]
 -- Note: your solution must be a one-liner. 
 ys :: [[Integer]]
-ys = undefined
+ys = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1]
 
 -- A rose tree is a tree that can have arbitray many of subtrees.
 -- In Haskell, we can define rose tree by the following.
