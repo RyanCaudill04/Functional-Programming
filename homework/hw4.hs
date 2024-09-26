dfef9271ce79738accc21158c92e67775e1a4234
Ryan Caudill <rcaudill@email.sc.edu>
2024-09-26 13:14:14 -0400

    Problem 1 and 2 hw4

---
+++ homework/hw4.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE InstanceSigs #-}
+-- Note: Your file must be free of typing errors. If your file can not
+-- be loaded into ghci, then you will get 0 point. Please read the instructions
+-- for each problem carefully. Failure to follow the instructions may result in
+-- 0 point.
+
+-- The so-called "applicative functor" is
+-- defined by the following type class. 
+class (Functor f) => Applicative' f where
+  pure' :: a -> f a 
+  app :: f (a -> b) -> f a -> f b
+
+-- Problem 1 (2 points). Show that Maybe is an instance
+-- of Applicative' by defining an instance declaration.
+
+instance Applicative' Maybe where
+  pure' = Just
+  app (Just f) (Just a) = Just (f a)
+  app f Nothing = Nothing
+  app Nothing a = Nothing
+
+    
+-- Problem 2 (2 points). Show that list [] is an instance
+-- of Applicative' by defining an instance declaration.
+
+instance Applicative' [] where
+  pure' a = [a]
+  app fs xs = [f x | f <- fs, x <- xs]
+
+
+-- Note: your definition must give the following
+-- results. 
+-- > pure' (+1) `app` [1,2,3,4]
+-- [2,3,4,5]
+-- > pure' (*) `app` [1,2,3,4] `app` [1,2]
+-- [1,2,2,4,3,6,4,8]
+
+
+-- Problem 3 (2 points). Define the following sequencing function
+-- using only pattern matching, recursion, pure' and app.
+sequenceA' :: Applicative' f => [f a] -> f [a]
+sequenceA' [] = pure' []
+sequenceA' (x:xs) = undefined
+
+
+-- Your definition must give the following results. 
+-- > sequenceA' [Just 1, Nothing, Just 2]
+-- Nothing
+-- > sequenceA' [Just 1, Just 3, Just 2]
+-- Just [1,3,2]
+
+
+-- Recall Monad type class.
+class (Functor m) => Monad' m where
+  return' :: a -> m a
+  bind' :: m a -> (a -> m b) -> m b
+
+-- Problem 4. (3 points). We can show a monad is also an applicative by the following.
+
+pure'' :: (Monad' f) => a -> f a
+pure'' a = return' a
+
+app'' :: (Monad' f) => f (a -> b) -> f a -> f b
+app'' f a = do
+  
